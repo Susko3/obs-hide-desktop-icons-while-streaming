92ac9b2aea012d859a64aa85db147eb7c93f3635
Susko3 <Susko3@protonmail.com>
2023-07-01 22:31:47 +0200

    Add copyright details

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+OBS Hide Desktop Icons While Streaming
+Copyright (C) Susko3 <susko3@protonmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
